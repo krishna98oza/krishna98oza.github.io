cedf7392c2c2cfa3a8d34a154580b2a8ea701f35
unknown <krish98oza.com>
2023-08-29 19:33:08 +0530

    half portfolio did

--- scripts.js
+++ scripts.js
@@ -16,3 +16,9 @@ window.addEventListener('load', () => {
   professionElement.classList.add('typing');
   typeProfession();
 });
+
+let resume = document.getElementById('resume-button-2');
+resume.addEventListener("click", () => {
+  window.open("https://drive.google.com/drive/u/3/folders/1fkJdx2FWop9rAZAXG8wAP3E5_a9QJ7Ly", "_blank");
+});
+
